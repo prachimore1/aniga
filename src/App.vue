2c1152b9e8f48e4b0c633573af57d8de58b106a0
prachi.more1 <prachi.more@integral.io>
2022-02-18 12:15:07 -0500

    Add Vuetify.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
   <WelcomeHeader/>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 import WelcomeHeader from "./components/Welcome-header";
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  name: "App",
+  name: 'App',
+
   components: {
     WelcomeHeader,
+    HelloWorld,
   },
-};
+
+  data: () => ({
+    //
+  }),
+}
 </script>
