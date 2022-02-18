9bc10cf024135e5681e8a999746aa36ae39e606a
prachi.more1 <prachi.more@integral.io>
2022-02-18 15:15:37 -0500

    Add image to the background.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <WelcomeHeader/>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+  <v-app class="pageBackground">
+      <WelcomeHeader/>
+    <v-card color="transparent" width="100%">
+      <v-img
+          class="text-center; imageStyle"
+          :src="logo"
+          contain
+          height="100%"
+          width="100%"
+      />
+    </v-card>
   </v-app>
 </template>
 
+<style>
+.pageBackground {
+  background-color: #FEC60D;
+}
+.imageStyle {
+  opacity: 60%
+}
+</style>
+
 <script>
 import WelcomeHeader from "./components/Welcome-header";
-import HelloWorld from './components/HelloWorld.vue'
+import logo from "./assets/WeddingFire.jpg";
 
 export default {
   name: 'App',
 
   components: {
     WelcomeHeader,
-    HelloWorld,
   },
 
   data: () => ({
-    //
+    logo: logo,
   }),
 }
 </script>
--- src/components/Welcome-header.vue
+++ src/components/Welcome-header.vue
@@ -1,14 +1,68 @@
 <template>
-  <h2 id="welcome-text">{{welcomeMsg}}</h2>
+  <v-app-bar class="barStyle">
+    <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-navigation-drawer
+        v-if="drawer"
+    >
+      <v-list
+          :items="items"
+      ></v-list>
+    </v-navigation-drawer>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <h1 class="font-weight-bol mb-3" id="welcome-text">{{ json.welcomeMsg }}</h1>
+      </v-col>
+    </v-row>
+  </v-app-bar>
 </template>
 
+<style>
+.barStyle {
+  /*background-color: #000000;*/
+}
+</style>
 <script>
 import json from "../assets/welcomeText.json";
 
 export default {
   name: "Welcome-header",
   data: function () {
-    return json;
+    return {
+      json: json,
+      items: [
+        {
+          title: 'Foo',
+          value: 'foo',
+        },
+        {
+          title: 'Bar',
+          value: 'bar',
+        },
+        {
+          title: 'Bar',
+          value: 'bar',
+        },
+        {
+          title: 'Bar',
+          value: 'bar',
+        },
+        {
+          title: 'Bar',
+          value: 'bar',
+        },
+        {
+          title: 'Bar',
+          value: 'bar',
+        }
+      ],
+      drawer: false,
+      group: null,
+    };
+  },
+  watch: {
+    group() {
+      this.drawer = false
+    },
   },
 };
 </script>
