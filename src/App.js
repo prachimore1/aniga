ecc24574bccae3091fb0d5dddaec914167de2a29
pmore5 <pmore5@ford.com>
2022-10-03 11:03:07 -0400

    Content sliding up after video timeout.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,34 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import video from "./Background.mp4";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
+AOS.init();
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
 class App extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {textDisplay: false};
@@ -15,19 +41,23 @@ class App extends React.Component {
     componentDidMount() {
         setTimeout(() => {
             this.setState({textDisplay: true})
+
         }, 3000);
     }
 
     render() {
         return (
             <div className="App">
-                {!this.state.textDisplay &&
-                    <video autoPlay muted loop>
+                    <video autoPlay muted loop >
                         <source src={video} type="video/mp4"/>
                     </video>
-                }
                 {this.state.textDisplay &&
-                    <div className="background">
+                    <div className="background"
+                         data-aos="slide-up"
+                         data-aos-offset="200"
+                         data-aos-duration="2000"
+                         data-aos-easing="ease-in-out"
+                    >
                         <section className="content one">
                             <h1>Section 1</h1>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
@@ -64,7 +94,8 @@ class App extends React.Component {
                         <section className="content">
                             <p className="room-content">Section 7</p>
                         </section>
-                    </div>}
+                    </div>
+                }
             </div>
         );
     }
