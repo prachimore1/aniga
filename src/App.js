666f69d485b094346aefecc106597070a26feb21
pmore5 <prachitempmore@gmail.com>
2022-10-04 16:07:16 -0400

    Replace background with thoran image.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import videoCover from "./Cover.mp4";
-import logo from "./logo.svg";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import thoran from "./thoran.png";
 
 AOS.init();
 AOS.init({
@@ -61,64 +61,71 @@ class App extends React.Component {
                     </div>
                 </div>
                 {this.state.textDisplay &&
-                    <div className="invite">
-                        <img src={logo} className="backImage" alt="logo"
-                             data-aos="slide-up"
-                             data-aos-offset="200"
-                             data-aos-duration="2000"
-                             data-aos-anchor-placement="top-bottom"
-                        />
-                        <div className="background"
-                             data-aos="slide-up"
-                             data-aos-offset="200"
-                             data-aos-duration="2000"
-                             data-aos-anchor-placement="top-bottom"
-                        >
-
-                            <section className="content one">
-                                <h1>Section 1</h1>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt
-                                    ut
-                                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                    ullamco
-                                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                    reprehenderit
-                                    in
-                                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                    cupidatat
-                                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            </section>
-
-                            <section className="content">
-                                <button className="viewButton">View</button>
-                            </section>
-
-                            <section className="content">
-                                <p className="room-content">Section 3</p>
-                            </section>
-
-                            <section className="content">
-                                <p className="room-content">Section 4</p>
-                            </section>
-
-                            <section className="content">
-                                <p className="room-content">Section 5</p>
-                            </section>
-
-                            <section className="content">
-                                <p className="room-content">Section 6</p>
-                            </section>
-
-                            <section className="content">
-                                <p className="room-content">Section 7</p>
-                            </section>
-                        </div>
-                    </div>
+                    <Invite/>
                 }
             </div>
         );
     }
 }
 
+function Invite() {
+    return (
+        <div className="invite">
+            <div className="background"
+                 data-aos="slide-up"
+                 data-aos-offset="200"
+                 data-aos-duration="2000"
+                 data-aos-anchor-placement="top-bottom"
+            >
+                <img src={thoran} alt="logo" className="thoran"/>
+            </div>
+            <div className="foreground"
+                 data-aos="slide-up"
+                 data-aos-offset="200"
+                 data-aos-duration="2000"
+                 data-aos-anchor-placement="top-bottom"
+            >
+
+                <section className="content one">
+                    <h1>Section 1</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                        incididunt
+                        ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit
+                        in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </section>
+
+                <section className="content">
+                    <button className="viewButton">View</button>
+                </section>
+
+                <section className="content">
+                    <p className="room-content">Section 3</p>
+                </section>
+
+                <section className="content">
+                    <p className="room-content">Section 4</p>
+                </section>
+
+                <section className="content">
+                    <p className="room-content">Section 5</p>
+                </section>
+
+                <section className="content">
+                    <p className="room-content">Section 6</p>
+                </section>
+
+                <section className="content">
+                    <p className="room-content">Section 7</p>
+                </section>
+            </div>
+        </div>
+    );
+}
 export default App;
